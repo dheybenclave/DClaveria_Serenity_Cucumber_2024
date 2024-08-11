ffd6cbb2df4a68ed1bce735903456ca735f8e095
Dhey Claveria <dheybenclave@gmail.com>
2024-08-11 16:58:01 +0800

    Added env variable path

--- src/test/java/utils/BaseClass.java
+++ src/test/java/utils/BaseClass.java
@@ -1,59 +1,57 @@
 package utils;
 
-import io.cucumber.java.AfterAll;
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import net.serenitybdd.annotations.Managed;
 import net.serenitybdd.core.pages.PageObject;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import stepdefinitions.ParameterDefinitions;
 
-public class BaseClass extends PageObject {
+public class BaseClass {
     public static Logger logger = LoggerFactory.getLogger(ParameterDefinitions.class);
-    WebDriver driver;
 
-    //
+    public WebDriver driver;
+
     @Before
-    void setup() {
+    public void setUp() throws Exception {
 //        WebDriverManager.chromedriver().setup();
-//        ChromeOptions options = new ChromeOptions();
-//        DesiredCapabilities capabilities = new DesiredCapabilities();
-//        System.setProperty("webdriver.chrome.driver", "./src/test/resources/windows/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "src/test/resources/webdriver/windows/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "./src/test/resources/webdriver/windows/chromedriver.exe");
 //        options.addArguments("--start-maximized");
+//        options.addArguments("start-maximized");
 //        options.addArguments("--incognito");
-//        options.addArguments(
-//                "--start-maximized", "--remote-allow-origins=*", "--test-type", "--no-sandbox", "--ignore-certificate-errors",
-//                "--incognito", "--disable-infobars", "--disable-gpu", "--disable-default-apps", "--disable-popup-blocking",
-//                "--disable-dev-shm-usage", "--disable-extensions", "--disable-web-security", "--disable-translate", "--disable-logging");
+//        options.addArguments("--start-maximized", "--remote-allow-origins=*", "--test-type", "--no-sandbox", "--ignore-certificate-errors", "--incognito", "--disable-infobars", "--disable-gpu", "--disable-default-apps", "--disable-popup-blocking", "--disable-dev-shm-usage", "--disable-extensions", "--disable-web-security", "--disable-translate", "--disable-logging");
 //
 //        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
 //        options.merge(capabilities);
-//        driver.manage().window().maximize();
 //        driver = new ChromeDriver(options);
 //        System.out.println("manage().window().maximize();");
-//        this.getDriver().manage().window().maximize();
+
     }
 
 
-    @AfterEach
+    @After
     public void tearDown() throws Exception {
-
-        this.getDriver().close();
-        driver.close();
-        System.out.println("Teardown Close Done");
-        this.getDriver().quit();
-        driver.quit();
-        System.out.println("Teardown Quit Done");
+//
+//        driver.close();
+//        System.out.println("Teardown Close Done");
+//        driver.quit();
+//        System.out.println("Teardown Quit Done");
 
     }
 
-    @AfterAll
+    @After
     public static void tearDownKiller() throws Exception {
-        // Kill the ChromeDriverService process
-//        Runtime.getRuntime().exec("taskkill /F /IM ChromeDriver.exe");
+//         Kill the ChromeDriverService process
+//        Runtime.getRuntime().exec("taskkill /F /IM chrome.exe");
 //        System.out.println("Teardown Kill Process Done");
     }
 }
